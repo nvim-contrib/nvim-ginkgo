ffe6fac27da982acbd1dd41d50ffe9d1d82b2737
Cenk Kılıç <cenk@kilic.dev>
2025-03-17 16:44:05 +0100

    feat(strategies): implement the initial strategy for dap
    fix: update setup call
    
    fix: update args
    
    fix: issue with reporting
    
    fix: update with escaping patterns
    
    fix: update escape pattern
    
    WIP: [
    
    Signed-off-by: Cenk Kılıç <cenk@kilic.dev>
    
    WIP: [
    
    Signed-off-by: Cenk Kılıç <cenk@kilic.dev>
    
    fix: remove the debug var

--- lua/nvim-ginkgo/utils.lua
+++ lua/nvim-ginkgo/utils.lua
@@ -227,9 +227,14 @@ function utils.create_position_focus(position)
 	local name = string.sub(position.id, string.find(position.id, "::") + 2)
 	name, _ = string.gsub(name, "::", " ")
 	name, _ = string.gsub(name, '"', "")
+	-- TODO: there is a problem with slashes for sure!
+	name, _ = string.gsub(name, "/", "\\/")
+	-- escape some pattern characters
+	name, _ = string.gsub(name, "([%.%+%-%*%?%[%]%(%)%$%^%|])", "\\%1")
+
 	-- prepare the pattern
 	-- https://github.com/onsi/ginkgo/issues/1126#issuecomment-1409245937
-	return "'\\b" .. name .. "\\b'"
+	return "\\b" .. name .. "\\b"
 end
 
 return utils
