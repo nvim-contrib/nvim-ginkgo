69593be5a4f0e97e39b21677cecdf236996f6d57
Svetlin Ralchev <iamralch@users.noreply.github.com>
2025-12-28 09:53:07 +0400

    refactor: replace setup_debugging() with check_dap_available()
    - update dap configuration initialization
    - improve error handling for missing dap-go plugin
    - adjust dap config parameters

--- lua/nvim-ginkgo/strategies/dap.lua
+++ lua/nvim-ginkgo/strategies/dap.lua
@@ -2,39 +2,29 @@ local M = {}
 
 local logger = require("neotest.logging")
 
----This will prepare and setup nvim-dap-go for debugging.
----@param cwd string
-function M.setup_debugging(cwd)
-	local ok, adapter = pcall(require, "dap-go")
+---Checks if dap-go is available for debugging.
+function M.check_dap_available()
+	local ok, _ = pcall(require, "dap-go")
 	if not ok then
-		local msg = "You must have leoluz/nvim-dap-go installed to use DAP strategy. "
-			.. "See the neotest-golang README for more information."
+		local msg = "You must have leoluz/nvim-dap-go installed to use DAP strategy."
 		logger.error(msg)
 		error(msg)
 	end
-
-	local opts = {
-		delve = {
-			cwd = cwd,
-		},
-	}
-	logger.debug({ "Setting up dap-go for DAP: ", opts })
-
-	adapter.setup(opts)
 end
 
---- @param path string
---- @param args string[]?
---- @return table | nil
-function M.get_dap_config(path, args)
+---@param cwd string
+---@param args string[]?
+---@return table
+function M.get_dap_config(cwd, args)
 	-- :help dap-configuration
 	local config = {
 		type = "go",
-		name = "Neotest-golang",
+		name = "nvim-ginkgo",
 		request = "launch",
 		mode = "test",
-		program = path,
+		program = cwd,
 		args = args,
+		cwd = cwd,
 		outputMode = "remote",
 	}
 
